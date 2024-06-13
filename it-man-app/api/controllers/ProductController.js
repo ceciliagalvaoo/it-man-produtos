b9dcff28516d6c3decddcfd4c8531040dc03d998
ceciliagalvaoo <Cecilia.Galvao@sou.inteli.edu.br>
2024-06-13 18:43:33 -0300

    Revert "🥅 the greatest obstacle to living is expectancy, which hangs upon tomorrow and loses today"
    This reverts commit 5c4ade918cc5fff1eb8a8ccd944920055c3374d5.

--- it-man-app/api/controllers/ProductController.js
+++ it-man-app/api/controllers/ProductController.js
@@ -7,74 +7,75 @@
 
 
 module.exports = {
-    create: async function (req, res) {
-        try {
-            const { name, description, price } = req.allParams();
+  create: async function (req, res) {
+      try {
+          const { name, description, price } = req.allParams();
 
-            const newProduct = await Product.create({
-                name,
-                description,
-                price
-            }).fetch();
+          const newProduct = await Product.create({
+              name,
+              description,
+              price
+          }).fetch();
 
-            return res.json(newProduct);
-        } catch (error) {
-            return res.serverError(error);
-        }
-    },
+          return res.json(newProduct);
+      } catch (error) {
+          return res.serverError(error);
+      }
+  },
 
-    // ITMAN: Complete a função find do ProductController
-    // Lembre-se de criar uma constante para armazenar o produto que você quer encontrar
-    // Lembre-se de retorná-lo como json
-    // Lembre-se de lidar com casos de erros
-    find: async function (req, res) {
-        try {
+  find: async function (req, res) {
+      try {
+          const products = await Product.find();
+          return res.json(products);
+      } catch (error) {
+          return res.serverError(error);
+      }
+  },
 
-        } catch (error) {
+  findOne: async function (req, res) {
+      try {
+          const productId = req.param('id');
+          const product = await Product.findOne({ id: productId });
+          if (!product) {
+              return res.notFound('Produto não encontrado');
+          }
+          return res.json(product);
+      } catch (error) {
+          return res.serverError(error);
+      }
+  },
 
-        }
-    },
+  update: async function (req, res) {
+      try {
+          const productId = req.param('id');
+          const { name, description, price } = req.allParams();
 
-    findOne: async function (req, res) {
-        try {
-            const productId = req.param('id');
-            const product = await Product.findOne({ id: productId });
-            if (!product) {
-                return res.notFound('Produto não encontrado');
-            }
-            return res.json(product);
-        } catch (error) {
-            return res.serverError(error);
-        }
-    },
+          const updatedProduct = await Product.updateOne({ id: productId }).set({
+              name,
+              description,
+              price
+          });
 
-    // ITMAN: Complete a função update do ProductController
-    // Dica: A função update usa o id do produto assim como a função delete ou a findOne, use de base
-    // Dica: A função update insere informações para um produto assim como a função create, use de base
-    update: async function (req, res) {
-        try {
+          if (!updatedProduct) {
+              return res.notFound('Produto não encontrado');
+          }
 
-            // Faça uma verificação aqui para caso o produto não seja encontrado
-            // if () {
-            //     return res.notFound('Produto não encontrado');
-            // }
+          return res.json(updatedProduct);
+      } catch (error) {
+          return res.serverError(error);
+      }
+  },
 
-            // return res.json(updatedProduct);
-        } catch (error) {
-            // return res.serverError(error);
-        }
-    },
-
-    delete: async function (req, res) {
-        try {
-            const productId = req.param('id');
-            const deletedProduct = await Product.destroyOne({ id: productId });
-            if (!deletedProduct) {
-                return res.notFound('Produto não encontrado');
-            }
-            return res.json(deletedProduct);
-        } catch (error) {
-            return res.serverError(error);
-        }
-    },
+  delete: async function (req, res) {
+      try {
+          const productId = req.param('id');
+          const deletedProduct = await Product.destroyOne({ id: productId });
+          if (!deletedProduct) {
+              return res.notFound('Produto não encontrado');
+          }
+          return res.json(deletedProduct);
+      } catch (error) {
+          return res.serverError(error);
+      }
+  },
 };
